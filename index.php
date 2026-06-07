39be03d8e04a91fc6d8bb1af14d805fa3e4fb36f
David Vicent <mapundadavid34@gmail.com>
2026-06-08 01:59:23 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+
+    <title>
+        Online Marketing Campaign Management System
+    </title>
+
+    <link rel="stylesheet"
+          href="assets/css/style.css">
+</head>
+
+<body>
+
+<header>
+
+<h1>
+Online Marketing Campaign Management System
+</h1>
+
+<nav>
+<a href="index.php">Home</a>
+<a href="login.php">Login</a>
+</nav>
+
+</header>
+
+<section>
+
+<h2>Welcome</h2>
+
+<p>
+Manage clients, campaigns,
+budgets and marketing performance.
+</p>
+
+</section>
+
+<footer>
+
+<p>
+© 2026 Online Marketing Campaign
+Management System
+</p>
+
+</footer>
+
+</body>
+
+</html>
