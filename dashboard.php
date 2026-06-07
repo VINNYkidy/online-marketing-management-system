09b93c9cc67dbb895b92af50fbf2c761f5b07772
David Vicent <mapundadavid34@gmail.com>
2026-06-08 02:50:20 +0300

    Update dashboard.php
    Improved dashboard UI with sidebar and statistics cards

--- dashboard.php
+++ dashboard.php
@@ -1,33 +1,59 @@
 <?php
-
+session_start();
 include("includes/config.php");
 
-$c1 = mysqli_num_rows(
-mysqli_query($conn,
-"SELECT * FROM clients")
-);
+if(!isset($_SESSION['user'])){
+    header("Location: login.php");
+}
+?>
 
-$c2 = mysqli_num_rows(
-mysqli_query($conn,
-"SELECT * FROM campaigns")
-);
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+<div class="sidebar">
+    <h2>OMCMS</h2>
+
+    <a href="dashboard.php">Dashboard</a>
+    <a href="modules/clients.php">Clients</a>
+    <a href="modules/campaigns.php">Campaigns</a>
+    <a href="modules/performance.php">Performance</a>
+    <a href="modules/reports.php">Reports</a>
+    <a href="logout.php">Logout</a>
+</div>
+
+<div class="main">
+
+<h1>Dashboard Overview</h1>
+
+<?php
+
+$clients = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM clients"));
+$campaigns = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM campaigns"));
 
 ?>
 
-<h1>Dashboard</h1>
+<div class="cards">
 
-<div>
-
+<div class="card">
 <h3>Total Clients</h3>
-
-<p><?php echo $c1; ?></p>
-
+<p><?php echo $clients; ?></p>
 </div>
 
-<div>
-
+<div class="card">
 <h3>Total Campaigns</h3>
-
-<p><?php echo $c2; ?></p>
+<p><?php echo $campaigns; ?></p>
+</div>
 
 </div>
+
+</div>
+
+</body>
+</html>
