a9f81daf8e1eb22c1d2edd1043be2990e062005c
David Vicent <mapundadavid34@gmail.com>
2026-06-08 02:01:01 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>Login</title>
+
+<link rel="stylesheet"
+href="assets/css/style.css">
+
+</head>
+
+<body>
+
+<h2>System Login</h2>
+
+<form>
+
+<label>Username</label>
+
+<input type="text"
+name="username">
+
+<br><br>
+
+<label>Password</label>
+
+<input type="password"
+name="password">
+
+<br><br>
+
+<button type="submit">
+Login
+</button>
+
+</form>
+
+</body>
+
+</html>
